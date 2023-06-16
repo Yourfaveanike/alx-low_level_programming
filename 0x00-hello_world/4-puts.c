efee8ed4839ab80b4319096f451be24f68591e81
Yourfaveanike <omotoyosishow@gmail.com>
2023-06-16 11:36:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - starts c program today
- * Return: 0
- */
+ * Return: 0 (sucessful)
+  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
