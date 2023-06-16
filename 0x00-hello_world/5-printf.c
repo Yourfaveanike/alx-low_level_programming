a2e0d07351fdb6015307e5160d6b70df9d344c48
Yourfaveanike <omotoyosishow@gmail.com>
2023-06-16 13:08:29 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase with proper grammar
+ * Return: 0 if sucessful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
